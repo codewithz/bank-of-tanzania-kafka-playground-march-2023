e1e49b1d61b7fa2ebb290bb502ba5eff6e45e8b5
Zartab M Nakhwa <zartab@codewithz.com>
2023-03-30 14:52:44 +0530

    Added Topic Configuration

---
+++ src/main/java/tz/bot/config/KafkaTopicConfig.java
@@ -0,0 +1,19 @@
+package tz.bot.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Bean
+    public NewTopic createTopicBOT(){
+        return TopicBuilder
+                .name("bank_of_tanzania")
+                .partitions(2)
+                .replicas(1)
+                .build();
+    }
+
+}
